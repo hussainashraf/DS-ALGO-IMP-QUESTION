0daf5981e635d5c513d485fb3059a7388c5406c2
hussain <hussainashraf>
2023-08-07 18:56:52 +0530

    DS

--- Sort012.java
+++ Sort012.java
@@ -7,8 +7,16 @@ public class Sort012 {
         for (int i = 0;i<arr.length;i++){
             System.out.print(arr[i]);
         }
-    }
-
+    }//Explaination We have to sort the array in constant space
+        //so for that We USE DUTCH NATIONAL FLAG ALGORITHM
+        // zero from 0 to low
+        // 1 from low to mid
+        // unknow from mid to high
+        // 2 from high to n
+        // we take 3 pointer low at 0 mid at 0 and high at last
+        // if 0 then we swap mid with low and mid++ and low++
+        // if 1 then we do mid++;
+        // if 2 we swap mid with high and high--
     public static void sort(int []arr){
         int low = 0;
         int high = arr.length-1;
@@ -16,6 +24,7 @@ public class Sort012 {
         int temp;
         while(mid<=high){
             switch (arr[mid]){
+                //swapping mid with low
                 case 0:{
                     temp = arr[low];
                     arr[low]=arr[mid];
@@ -24,10 +33,13 @@ public class Sort012 {
                     low++;
                     break;
                 }
+                // no swapping
                 case 1:{
                     mid++;
                     break;
                 }
+
+                //swapping mid with high
                 case 2:{
                     temp = arr[mid];
                     arr[mid] = arr[high];
